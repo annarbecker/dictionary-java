0cd26e89d9b0bf71c98ff5823eea9aa12fc27a14
Anna Becker <anna.r.becker@gmail.com>
2016-02-19 10:53:27 -0800

    Add styling and formatting to index and add definition pages.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -21,7 +21,7 @@ public class AppTest extends FluentTest {
   @Test
   public void rootTest() {
       goTo("http://localhost:4567/");
-      assertThat(pageSource()).contains("Dictionary");
+      assertThat(pageSource()).contains("Add Word");
   }
 
   @Test
